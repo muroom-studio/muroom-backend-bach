3260637105c71a0bad7ff7532353425713e8dc86
monte-kim <monte6198@gmail.com>
2025-10-19 01:02:48 +0900

    Initial commit: Spring Boot project setup for muroom-backend-bach

---
+++ src/main/java/kr/muroom/muroombackendbach/MuroomBackendBachApplication.java
@@ -0,0 +1,13 @@
+package kr.muroom.muroombackendbach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuroomBackendBachApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MuroomBackendBachApplication.class, args);
+  }
+
+}
---
+++ src/test/java/kr/muroom/muroombackendbach/MuroomBackendBachApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.muroom.muroombackendbach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuroomBackendBachApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
