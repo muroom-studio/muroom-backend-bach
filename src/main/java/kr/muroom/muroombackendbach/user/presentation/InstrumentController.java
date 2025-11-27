175f0d09218f14957da98daaf87bbf854d8afbe2
2-say <dev2say@gmail.com>
2025-11-28 00:15:49 +0900

    fix: 누락된 Instrument 추가

--- src/main/java/kr/muroom/muroombackendbach/user/presentation/InstrumentController.java
+++ src/main/java/kr/muroom/muroombackendbach/user/presentation/InstrumentController.java
@@ -1,6 +1,7 @@
 package kr.muroom.muroombackendbach.user.presentation;
 
 import java.util.List;
+import kr.muroom.muroombackendbach.common.presentation.response.ApiResponse;
 import kr.muroom.muroombackendbach.user.application.InstrumentService;
 import kr.muroom.muroombackendbach.user.presentation.dto.InstrumentDto.InstrumentResponse;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ public class InstrumentController {
    * 악기 전체 목록 조회
    */
   @GetMapping
-  public List<InstrumentResponse> getAllInstruments() {
-    return instrumentService.getAllInstruments();
+  public ApiResponse<List<InstrumentResponse>> getAllInstruments() {
+    return ApiResponse.success(instrumentService.getAllInstruments());
   }
 }
