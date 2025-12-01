fd80f8452c2747677904f8dda6d2c540c18e649b
2-say <dev2say@gmail.com>
2025-12-01 22:56:38 +0900

    feat: 약관 ID 리스트 조회, 약관 내용 조회 분리

--- src/main/java/kr/muroom/muroombackendbach/terms/application/TermService.java
+++ src/main/java/kr/muroom/muroombackendbach/terms/application/TermService.java
@@ -1,8 +1,11 @@
 package kr.muroom.muroombackendbach.terms.application;
 
+import static kr.muroom.muroombackendbach.terms.presentation.dto.TermDto.*;
+
+import kr.muroom.muroombackendbach.terms.domain.entity.TermContent;
 import kr.muroom.muroombackendbach.terms.domain.entity.TermsType;
+import kr.muroom.muroombackendbach.terms.domain.repository.TermContentRepository;
 import kr.muroom.muroombackendbach.terms.domain.repository.TermRepository;
-import kr.muroom.muroombackendbach.terms.presentation.dto.TermDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,13 +15,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TermService {
 
-    private final TermRepository termRepository;
+  private final TermRepository termRepository;
+  private final TermContentRepository termContentRepository;
 
-    public List<TermDto.TermsWithContentDto> getTermsMusicianByType(List<TermsType> types) {
-        return termRepository.findLatestTermsByRoleAndTypes("MUSICIAN", types);
-    }
+  public List<TermsWithContentDto> getTermsMusicianByType(List<TermsType> types) {
+    return termRepository.findLatestTermsByRoleAndTypes("MUSICIAN", types);
+  }
 
-    public List<TermDto.TermsWithContentDto> getTermsOwnerByType(List<TermsType> types) {
-        return termRepository.findLatestTermsByRoleAndTypes("OWNER", types);
-    }
+  public List<TermsWithContentDto> getTermsOwnerByType(List<TermsType> types) {
+    return termRepository.findLatestTermsByRoleAndTypes("OWNER", types);
+  }
+
+  public TermContentDto getTermContent(Long termId) {
+    TermContent termContent = termContentRepository.findById(termId).orElseThrow();
+    return new TermContentDto(termContent.getTerm().getId(), termContent.getContent());
+  }
 }
--- src/main/java/kr/muroom/muroombackendbach/terms/domain/repository/TermQueryRepository.java
+++ src/main/java/kr/muroom/muroombackendbach/terms/domain/repository/TermQueryRepository.java
@@ -1,10 +1,13 @@
 package kr.muroom.muroombackendbach.terms.domain.repository;
 
+import static kr.muroom.muroombackendbach.terms.presentation.dto.TermDto.*;
+
 import kr.muroom.muroombackendbach.terms.domain.entity.TermsType;
 import kr.muroom.muroombackendbach.terms.presentation.dto.TermDto;
 
 import java.util.List;
 
 public interface TermQueryRepository {
-    List<TermDto.TermsWithContentDto> findLatestTermsByRoleAndTypes(String role, List<TermsType> types);
+
+  List<TermsWithContentDto> findLatestTermsByRoleAndTypes(String role, List<TermsType> types);
 }
