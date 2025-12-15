6fb12982b585faa5bde0e31b4e43898ae02d42c3
2-say <dev2say@gmail.com>
2025-12-15 23:25:24 +0900

    feat: PATCH 메서드 CORS 추가

--- src/main/java/kr/muroom/muroombackendbach/common/config/WebConfig.java
+++ src/main/java/kr/muroom/muroombackendbach/common/config/WebConfig.java
@@ -24,7 +24,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins(allowedOrigins)
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true);
   }
