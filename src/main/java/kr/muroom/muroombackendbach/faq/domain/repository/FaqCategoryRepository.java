a5cbece318fe88e651dfd053ef89b80dcaac5eda
2-say <dev2say@gmail.com>
2025-12-21 01:06:37 +0900

    feat: faq 카테고리 조회 API 추가

--- src/main/java/kr/muroom/muroombackendbach/faq/domain/repository/FaqCategoryRepository.java
+++ src/main/java/kr/muroom/muroombackendbach/faq/domain/repository/FaqCategoryRepository.java
@@ -1,8 +1,10 @@
 package kr.muroom.muroombackendbach.faq.domain.repository;
 
+import java.util.List;
 import kr.muroom.muroombackendbach.faq.domain.entity.FaqCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FaqCategoryRepository extends JpaRepository<FaqCategory, Long> {
 
+  List<FaqCategory> findAllByIsActiveTrueOrderByIdAsc();
 }
--- src/main/java/kr/muroom/muroombackendbach/faq/presentation/FaqController.java
+++ src/main/java/kr/muroom/muroombackendbach/faq/presentation/FaqController.java
@@ -1,7 +1,5 @@
 package kr.muroom.muroombackendbach.faq.presentation;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.muroom.muroombackendbach.common.presentation.response.ApiResponse;
 import kr.muroom.muroombackendbach.common.presentation.response.PaginatedData;
 import kr.muroom.muroombackendbach.faq.application.FaqService;
@@ -12,21 +10,24 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "FAQ API", description = "등록된 FAQ 페이지를 가져옵니다.")
 @RestController
 @RequestMapping("/api/v1/faqs")
 @RequiredArgsConstructor
-public class FaqController {
+public class FaqController implements FaqControllerDocs {
 
   private final FaqService faqService;
 
   @GetMapping
-  public ApiResponse<PaginatedData<FaqResponse>> getAllFaqs(
-      @Parameter(hidden = true)
+  public ApiResponse<PaginatedData<FaqResponse>> getFaqs(
+      @RequestParam(name = "keyword", required = false) String keyword,
+      @RequestParam(name = "categoryId", required = false) Long categoryId,
       @PageableDefault(sort = "createdAt", direction = Direction.DESC) Pageable pageable
   ) {
-    return ApiResponse.success(PaginatedData.from(faqService.getAllFaqs(pageable)));
+    return ApiResponse.success(
+        PaginatedData.from(faqService.findFaqs(keyword, categoryId, pageable))
+    );
   }
 }
