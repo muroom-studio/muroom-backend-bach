1c189f6061372a55897786afe31340fa9419187a
2-say <dev2say@gmail.com>
2025-12-30 22:07:43 +0900

    feat: 사용하지 않는 클래스 삭제

--- src/main/java/kr/muroom/muroombackendbach/user/domain/repository/OwnerRepository.java
+++ src/main/java/kr/muroom/muroombackendbach/user/domain/repository/OwnerRepository.java
@@ -15,8 +15,5 @@ public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
   Optional<Owner> findByPhoneNumber(String phoneNumber);
 
-  @Query(value = "SELECT nextval('owner_id_seq')", nativeQuery = true)
-  Long getNextIdSequence();
-
   Boolean existsByPhoneNumber(String phoneNumber);
 }
