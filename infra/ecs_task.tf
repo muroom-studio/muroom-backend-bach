c223285caa9feb594bbb680c02b6c6e6f1a9d42f
monte-kim <monte6198@gmail.com>
2026-02-04 19:21:48 +0900

    deploy: prod-v26.02.04.A

--- infra/ecs_task.tf
+++ infra/ecs_task.tf
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "muroom_ecs_task_definition_prod" {
   container_definitions = jsonencode([
     {
       name         = "muroom-backend-bach-container-prod"
-      image        = "${aws_ecr_repository.muroom_backend_bach.repository_url}:prod-latest" #prod-v26.02.04/A"
+      image        = "${aws_ecr_repository.muroom_backend_bach.repository_url}:prod-v26.02.04.A"
       cpu          = 2048
       memory       = 3072
       essential    = true
